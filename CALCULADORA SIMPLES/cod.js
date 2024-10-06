247dca1c429b0282813f94a8064ca573be431ff8
Jean Carlos <126817643+JEAN-CARLOS2@users.noreply.github.com>
2024-10-06 15:02:43 -0400

    Add files via upload

--- CALCULADORA SIMPLES/cod.js
+++ CALCULADORA SIMPLES/cod.js
@@ -3,8 +3,11 @@ function soma(){
    const n1 = parseFloat(document.getElementById("n1").value);
    const n2 = parseFloat(document.getElementById("n2").value);
 
-   
+   if(n1 || n2 != Number){
+    alert("Digite apenas números!");
+   }else{
    const res = n1 +n2;
 
     alert("O resultado é: " + res);
+ }
 }
